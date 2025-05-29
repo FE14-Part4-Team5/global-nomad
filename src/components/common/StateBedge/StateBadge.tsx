0b2d29c4e875016d2df50da9799bcb570060f3da
최권진 <rnjswls060@naver.com>
2025-05-29 15:14:47 +0900

    클래스네임 수정을 위한 코드 추가 및 css 클래스네임 수정

--- src/components/common/StateBedge/StateBadge.tsx
+++ src/components/common/StateBedge/StateBadge.tsx
@@ -1,10 +1,12 @@
 import styles from './StateBadge.module.css';
 
 const StateBadge = ({ status, label }: StateBadgeProps) => {
+  const capital = capitalize(status);
+
   return (
     <span role="status">
-      <div className={`${styles['bedge-common']} ${styles[`bedge-${status}`]}`}>
-        <div className={`${styles['label-common']} ${styles[status]}`}>{label}</div>
+      <div className={`${styles.bedgeCommon} ${styles[`bedge${capital}`]}`}>
+        <div className={`${styles.labelCommon} ${styles[`label${capital}`]}`}>{label}</div>
       </div>
     </span>
   );
@@ -16,3 +18,5 @@ type StateBadgeProps = {
   status: 'pending' | 'confirmed' | 'declined' | 'canceled' | 'completed';
   label: string;
 };
+
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
