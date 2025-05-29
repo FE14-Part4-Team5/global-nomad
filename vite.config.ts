6c1eff08c1f82a37d8e4436cb71aff875f137d6d
ghdtnals <hongsumin58@gmail.com>
2025-05-29 16:17:34 +0900

    chore: 경로 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
+import tsPathsConfig from './tsconfig.paths.json';
+
+const aliases = Object.entries(tsPathsConfig.compilerOptions.paths).map(([key, [value]]) => ({
+  find: key.replace('/*', ''),
+  replacement: path.resolve(__dirname, value.replace('/*', '')),
+}));
+
+//* https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: aliases,
+  },
 });
