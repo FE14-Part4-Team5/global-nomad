6ed8c1eb7e4af44e3058903a45e2e8e5ecf9a5c3
최권진 <rnjswls060@naver.com>
2025-05-29 15:53:28 +0900

    app.tsx delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import StateBadge from './components/StateBedge/StateBadge';
-
 function App() {
   return (
     <>
-      <div>
-        <StateBadge status="pending" label="예약 승인" />
-      </div>
+      <div></div>
     </>
   );
 }
