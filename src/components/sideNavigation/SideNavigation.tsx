dda17a24a8f3266bc9d116e8d078d0068dd544ce
ShiroUsagi25 <irrukaboy@gmail.com>
2025-06-01 18:57:24 +0900

    feature: 파일명 수정 및 개선

--- src/components/sideNavigation/SideNavigation.tsx
+++ src/components/sideNavigation/SideNavigation.tsx
@@ -1,29 +1,20 @@
-// src/components/SideNavigation/SideNavigation.tsx
-
 import React from "react";
 import { Link } from "react-router-dom";
-import "./SideNavigation.css";
-import profileIconSm from "../../assets/icons/profile_size=sm.svg";
-import profileIconMd from "../../assets/icons/profile_size=md.svg";
-import profileIconLg from "../../assets/icons/profile_size=lg.svg";
-import ProfileImageEditor from "../profileImageEditor/ProfileImageEditor";
+import ProfileImage from "../ProfileImage/ProfileImage";
+import "./sideNavigation.css";
 
 const SideNavigation: React.FC = () => {
   return (
-    <aside className="side-navigation">
-      <div className="profile-container">
-        <picture>
-          <source media="(max-width: 767px)" srcSet={profileIconSm} />
-          <source media="(max-width: 1024px)" srcSet={profileIconMd} />
-          <ProfileImageEditor initialImage={profileIconLg} />
-        </picture>
-        <h2 className="user-name">사용자 이름</h2>
+    <aside className="sideNavigation">
+      <div className="profileContainer">
+        <ProfileImage />
+        <h2 className="userName">사용자 이름</h2>
       </div>
-      <nav className="menu-items">
-        <Link to="/my-info" className="menu-button">내 정보</Link>
-        <Link to="/reservations" className="menu-button">예약 내역</Link>
-        <Link to="/my-experiences" className="menu-button">내 체험 관리</Link>
-        <Link to="/reservation-status" className="menu-button">예약 현황</Link>
+      <nav className="menuItems">
+        <Link to="/my-info" className="menuButton">내 정보</Link>
+        <Link to="/reservations" className="menuButton">예약 내역</Link>
+        <Link to="/my-experiences" className="menuButton">내 체험 관리</Link>
+        <Link to="/reservation-status" className="menuButton">예약 현황</Link>
       </nav>
     </aside>
   );
