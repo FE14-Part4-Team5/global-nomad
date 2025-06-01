cbf0a487ae04e6280b3b9b0d71aa3c343f8a2f60
최권진 <rnjswls060@naver.com>
2025-06-01 19:30:25 +0900

    Card 작업 완료

---
+++ src/components/MyExperienceCard/MyExperienceCard.tsx
@@ -0,0 +1,56 @@
+import styles from './MyExperienceCard.module.css';
+
+import svg from '../../assets/icons/icon_active=0n.svg';
+
+const MyExperienceCard = ({
+  bannerImageUrl,
+  title,
+  rating,
+  reviewCount,
+  currencySymbol,
+  price,
+  priceUnit,
+  editButton,
+  deleteButton,
+}: MyExperienceCardProps) => {
+  return (
+    <div>
+      <div className={styles.card}>
+        <div className={styles.contents}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.ratingReview}>
+            <img src={svg} alt="star svg" className={styles.star} />
+            <div className={styles.rating}>{(rating ?? 0).toFixed(1)}</div>
+            <div className={styles.reviewCount}>({reviewCount})</div>
+          </div>
+          <div className={styles.priceGroup}>
+            <div className={styles.currencySymbol}>{currencySymbol}</div>
+            <div className={styles.price}>{price.toLocaleString()}</div>
+            <div className={styles.priceUnit}>{priceUnit}</div>
+          </div>
+          <div className={styles.buttons}>
+            <span>{editButton}</span>
+            <span>{deleteButton}</span>
+          </div>
+        </div>
+        <div>
+          <img src={bannerImageUrl} alt={`체험 배너 - ${title}`} className={styles.img} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyExperienceCard;
+
+type MyExperienceCardProps = {
+  title: string;
+  rating: number;
+  reviewCount: number;
+  price: number;
+  priceUnit: string;
+  currencySymbol: string;
+  bannerImageUrl: string;
+  editButton: React.ReactNode;
+  deleteButton: React.ReactNode;
+};
---
+++ src/components/ReservationCard/ReservationCard.tsx
@@ -0,0 +1,123 @@
+import styles from './ReservationCard.module.css';
+
+import StateBadge from '../StateBedge/StateBadge';
+
+const ReservationCard = ({
+  activity,
+  status,
+  date,
+  dateDot,
+  startTime,
+  timedash,
+  endTime,
+  currencySymbol,
+  totalPrice,
+  headCount,
+  headCountUnit,
+  reviewSubmitted,
+  reviewSubmittedButton,
+  editReservationButton,
+  cancelReservationButton,
+}: ReservationCardProps) => {
+  return (
+    <div>
+      <div className={styles.card}>
+        <div>
+          <div className={styles.dateTabletMobile}>{date}</div>
+          <div className={styles.contentsImageCard}>
+            <div className={styles.contents}>
+              <div>
+                <StateBadge status={status} label={statusLabelMap[status]} />
+              </div>
+              <div className={styles.title}>{activity.title}</div>
+              <div className={styles.time}>
+                <div className={styles.datePc}>{date}</div>
+                <div className={styles.dateDotPc}>{dateDot}</div>
+                <div>{startTime}</div>
+                <div>{timedash}</div>
+                <div>{endTime}</div>
+              </div>
+              <div className={styles.priceGroup}>
+                <div className={styles.priceUnit}>
+                  <div className={styles.currencySymbol}>{currencySymbol}</div>
+                  <div className={styles.totalPrice}>{totalPrice}</div>
+                  <div className={styles.headCount}>{headCount}</div>
+                  <div className={styles.headCountUnit}>{headCountUnit}</div>
+                </div>
+                <div className={styles.buttonsPc}>
+                  {status === 'pending' ? (
+                    <div className={styles.editCancleButton}>
+                      <div>{editReservationButton}</div>
+                      <div>{cancelReservationButton}</div>
+                    </div>
+                  ) : null}
+                  {!reviewSubmitted && status === 'completed' ? reviewSubmittedButton : null}
+                </div>
+              </div>
+            </div>
+            <div className={styles.imageParents}>
+              <img
+                src={activity.bannerImageUrl}
+                alt={`배너 이미지- ${activity.title}`}
+                className={styles.image}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonsTabletMobile}>
+          {status === 'pending' ? (
+            <div className={styles.editCancleButton}>
+              <div>{editReservationButton}</div>
+              <div>{cancelReservationButton}</div>
+            </div>
+          ) : null}
+          <div className={styles.reviewSubmittedButton}>
+            {!reviewSubmitted && status === 'completed' ? reviewSubmittedButton : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReservationCard;
+
+type ReservationCardProps = MyReservation & {
+  currencySymbol: string;
+  headCountUnit: string;
+  editReservationButton: React.ReactNode;
+  cancelReservationButton: React.ReactNode;
+  reviewSubmittedButton: React.ReactNode;
+  timedash: string;
+  dateDot: string;
+};
+
+type MyReservation = {
+  id?: number;
+  teamId?: string;
+  userId?: number;
+  activity: {
+    id: number;
+    bannerImageUrl: string;
+    title: string;
+  };
+  status: ReservationStatus;
+  reviewSubmitted: boolean;
+  totalPrice: number;
+  headCount: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
+
+type ReservationStatus = 'pending' | 'confirmed' | 'declined' | 'canceled' | 'completed';
+
+const statusLabelMap = {
+  pending: '예약 완료',
+  confirmed: '예약 승인',
+  declined: '예약 거절',
+  canceled: '예약 취소',
+  completed: '체험 완료',
+};
