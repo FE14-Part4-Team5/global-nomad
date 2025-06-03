bae644c5397977b7b4ef5a293a2dfb686a372386
최권진 <rnjswls060@naver.com>
2025-06-03 11:11:25 +0900

    svg아이콘 삭제 및 코드 수정

--- src/components/side-navigation/parts/SideNavItem.tsx
+++ src/components/side-navigation/parts/SideNavItem.tsx
@@ -1,24 +1,17 @@
 import { Link, useLocation } from 'react-router-dom';
+import personIcon from '@/assets/icons/icon_user.svg?react';
+import listIcon from '@/assets/icons/icon_list.svg?react';
+import settingIcon from '@/assets/icons/icon_setting.svg?react';
+import calendarIcon from '@/assets/icons/icon_calendar.svg?react';
+
 import styles from './SideNavItem.module.css';
-
-import personIcon from '@/assets/icons/icon_user.svg';
-import activePersonIcon from '@/assets/icons/icon_user_active.svg';
-
-import listIcon from '@/assets/icons/icon_list.svg';
-import activeListIcon from '@/assets/icons/icon_list_active.svg';
-
-import settingIcon from '@/assets/icons/icon_setting.svg';
-import activeSettingIcon from '@/assets/icons/icon_setting_active.svg';
-
-import calendarIcon from '@/assets/icons/icon_calendar.svg';
-import activeCalenderIcon from '@/assets/icons/icon_calendar_active.svg';
 
 const SideNavItem = () => {
   const location = useLocation();
 
   return (
     <div>
-      {navItems.map(({ path, label, icon, activeIcon }) => {
+      {navItems.map(({ path, label, Icon }) => {
         const isActive = location.pathname === path;
         return (
           <Link
@@ -26,7 +19,7 @@ const SideNavItem = () => {
             to={path}
             className={isActive ? styles.activeContentsBox : styles.contentsBox}
           >
-            <img src={isActive ? activeIcon : icon} alt={`${label} icon`} className={styles.icon} />
+            <Icon className={`${styles.icon} ${isActive ? styles.activeIcon : ''}`} />
             <div className={styles.itemText}>{label}</div>
           </Link>
         );
@@ -41,25 +34,21 @@ const navItems = [
   {
     path: '/my-profile',
     label: '내 정보',
-    icon: personIcon,
-    activeIcon: activePersonIcon,
+    Icon: personIcon,
   },
   {
     path: '/reservation-list',
     label: '예약내역',
-    icon: listIcon,
-    activeIcon: activeListIcon,
+    Icon: listIcon,
   },
   {
     path: '/my-experiences',
     label: '내 체험 관리',
-    icon: settingIcon,
-    activeIcon: activeSettingIcon,
+    Icon: settingIcon,
   },
   {
     path: '/reservation-status',
     label: '예약 현황',
-    icon: calendarIcon,
-    activeIcon: activeCalenderIcon,
+    Icon: calendarIcon,
   },
 ];
