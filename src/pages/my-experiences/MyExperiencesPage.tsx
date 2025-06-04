71af2f578367b9c6ddf82e6f9cf81700c3a3268c
최권진 <rnjswls060@naver.com>
2025-06-05 01:26:13 +0900

    데이터 없는 경우 보여질 컴포넌트 (공통으로 승격예정)구현

--- src/pages/my-experiences/MyExperiencesPage.tsx
+++ src/pages/my-experiences/MyExperiencesPage.tsx
@@ -1,4 +1,5 @@
 import SideNavigation from '@/components/side-navigation/SideNavigation';
+import MyExperiencesHeader from './components/my-experiences-header/MyExperiencesHeader';
 import MyExperienceCard from '@/components/my-experience-card/MyExperienceCard';
 import MyExperiencesButton from './components/my-experiences-button/MyExperiencesButton';
 import ExampleLogin from './example/ExampleLogin';
@@ -7,9 +8,9 @@ import { useMyProfile } from '@/hooks/useMyProfile';
 import { useMyActivities } from '@/hooks/useMyActivities';
 
 import type { MyExperienceCardProps } from '@/components/my-experience-card/MyExperienceCard';
-import EmptyStateImage from '@/assets/images/img_empty.png';
 import styles from './MyExperiencesPage.module.css';
-import MyExperiencesHeader from './components/my-experiences-header/MyExperiencesHeader';
+import EmptyState from './components/empty-state/EmptyState';
+import { Link } from 'react-router-dom';
 
 const MyExperiences = () => {
   const teamId = 'team5';
@@ -34,19 +35,16 @@ const MyExperiences = () => {
         {userData && <SideNavigation defaultImage={userData.profileImageUrl} />}
       </div>
       <div>
-        <MyExperiencesHeader />
-        {userActivities?.activities.length === 0 && (
-          <div className={styles.emptyState}>
-            <div className={styles.emptyStateImageWrapper}>
-              <img
-                src={EmptyStateImage}
-                alt="등록된 체험이 없어 아쉬워하는 지구 이미지"
-                className={styles.emptyStateImage}
-              />
-            </div>
-            <div className={styles.emptyStateText}>아직 등록한 체험이 없어요</div>
-          </div>
-        )}
+        <MyExperiencesHeader
+          title="내 체험 관리"
+          subTitle="체험을 등록하거나 수정 및 삭제가 가능합니다."
+          children={
+            <Link to={'/add'}>
+              <button className={styles.button}>체험 등록하기</button>
+            </Link>
+          }
+        />
+        {userActivities?.activities.length === 0 && <EmptyState text="아직 등록한 체험이 없어요" />}
         <div className={styles.card}>
           {userActivities?.activities.map((item: MyExperienceCardProps) => (
             <MyExperienceCard
--- src/pages/my-experiences/components/my-experiences-header/MyExperiencesHeader.tsx
+++ src/pages/my-experiences/components/my-experiences-header/MyExperiencesHeader.tsx
@@ -1,20 +1,21 @@
-import { Link } from 'react-router-dom';
-
 import styles from './MyExperiencesHeader.module.css';
 
-const MyExperiencesHeader = () => {
+const MyExperiencesHeader = ({
+  title,
+  subTitle,
+  children,
+}: {
+  title: string;
+  subTitle: string;
+  children?: React.ReactElement;
+}) => {
   return (
     <div className={styles.contents}>
       <div className={styles.headerText}>
-        <div className={styles.title}>내 체험 관리</div>
-        <div className={styles.subTitle}>
-          체험을 등록하거나 <br className={styles.br} />
-          수정 및 삭제가 가능합니다.
-        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
       </div>
-      <Link to={'/add'}>
-        <button className={styles.button}>체험 등록하기</button>
-      </Link>
+      {children}
     </div>
   );
 };
