3c3856a05aee45d16a90d27a1cbaca02a7d8fa4e
BANGHoYeong <bhy13579@gmail.com>
2025-06-07 01:54:10 +0900

    router 오류 해결

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from './components/layout/main-layout/MainLayout';
 
-
 import ReservationList from './pages/reservation-list/ReservationListPage';
-
 
 import LoginPage from './pages/login/LoginPage';
 import SignupPage from './pages/signup/SignupPage';
 
 import MyExperiencesPage from './pages/my-experiences/MyExperiencesPage';
-
 
 const router = createBrowserRouter([
   {
@@ -23,17 +20,16 @@ const router = createBrowserRouter([
     ],
   },
   {
-
     path: '/reservation-list',
     element: <ReservationList />,
-
+  },
+  {
     path: '/login',
     element: <LoginPage />,
   },
   {
     path: '/signup',
     element: <SignupPage />,
-
   },
 ]);
 
