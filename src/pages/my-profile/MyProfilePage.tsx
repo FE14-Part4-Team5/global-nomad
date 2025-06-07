287da9bf1a6b2d78f0389ea4400677a820b17f86
Jiwon42 <wjdwldnjs145@gmail.com>
2025-06-07 19:28:04 +0900

    fix: 모바일에서 사이드 네비게이션 바 조건부 렌더링 및 정보수정폼 상태 처리 수정

--- src/pages/my-profile/MyProfilePage.tsx
+++ src/pages/my-profile/MyProfilePage.tsx
@@ -3,31 +3,60 @@ import profileImg from '@/assets/icons/profile_size=lg.svg';
 import styles from './MyProfilePage.module.css';
 import ProfileForm from './components/ProfileForm';
 import type { MyProfileFormValues } from '@/hooks/useMyProfileUpdateForm';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const MyProfilePage = () => {
-  const navigate = useNavigate();
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === '/my-profile' && !isDesktop) {
+      setIsEdit(true);
+    }
+  }, [location.pathname, isDesktop]);
+
   const handleProfileSubmit = async (data: MyProfileFormValues) => {
     console.log(`제출 완료 ${data.nickname} ${data.newPassword} ${data.newConfirmPassword}`);
+    setIsEdit(false);
   };
 
   const handleCancelUpdate = () => {
-    //임시
     console.log('취소');
-    navigate(-1);
+    setIsEdit(false);
   };
+
   const handleProfileImageUpload = (file: File) => {
     console.log('이미지 업로드:', file);
   };
+
   return (
     <div className={styles.container}>
-      <div className={styles.sideNavigationWrapper}>
-        <SideNavigation defaultImage={profileImg} onImageUpload={handleProfileImageUpload} />
-      </div>
-      <div className={styles.profileFormWrapper}>
-        <ProfileForm onClick={handleCancelUpdate} onSubmit={handleProfileSubmit} />
-      </div>
+      {(isDesktop || !isEdit) && (
+        <div className={styles.sideNavigationWrapper}>
+          <SideNavigation
+            defaultImage={profileImg}
+            onImageUpload={handleProfileImageUpload}
+            onNavItemClick={() => setIsEdit(true)}
+          />
+        </div>
+      )}
+      {(isDesktop || isEdit) && (
+        <div className={styles.profileFormWrapper}>
+          <ProfileForm onClick={handleCancelUpdate} onSubmit={handleProfileSubmit} />
+        </div>
+      )}
     </div>
   );
 };
+
 export default MyProfilePage;
