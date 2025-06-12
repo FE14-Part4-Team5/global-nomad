89d11304deea834b7b3d5289618d542d1b05e247
jihoon kwak <kwakjihoon1001@gmail.com>
2025-06-13 01:20:10 +0900

    fix: 브랜치 push를 위한 임시 수정

--- src/pages/reservation-status/ReservationStatus.tsx
+++ src/pages/reservation-status/ReservationStatus.tsx
@@ -1,7 +1,7 @@
 import styles from './ReservationStatus.module.css';
 
 const ReservationStatus = () => {
-  return <div className={styles.page}>예약현황페이지</div>;
+  return <div className={styles.page}>예약현황페이지입니다.</div>;
 };
 
 export default ReservationStatus;
