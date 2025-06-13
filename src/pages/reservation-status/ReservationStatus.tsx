e585d2a31bdcfd6f77b7afb0bd262acc542249b3
jihoon135 <kwakjihoon1001@gmail.com>
2025-06-13 20:45:55 +0900

    feat: 예약 현황 페이지 UI 제작

--- src/pages/reservation-status/ReservationStatus.tsx
+++ src/pages/reservation-status/ReservationStatus.tsx
@@ -1,7 +1,26 @@
+import SideNavigation from '@/components/side-navigation/SideNavigation';
 import styles from './ReservationStatus.module.css';
+import profileImg from '@/assets/icons/profile_size=lg.svg';
+import { Calendar } from './components/Calendar';
 
 const ReservationStatus = () => {
-  return <div className={styles.page}>예약현황페이지입니다.</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.navContainer}>
+        <SideNavigation defaultImage={profileImg} />
+      </div>
+      <div className={styles.calendarWrapper}>
+        <div className={styles.header}>
+          <div className={styles.title}>예약 현황</div>
+          <div className={styles.explanation}>
+            내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.
+          </div>
+        </div>
+        {/*예약 리스트 드롭다운*/}
+        <Calendar />
+      </div>
+    </div>
+  );
 };
 
 export default ReservationStatus;
---
+++ src/pages/reservation-status/components/Calendar.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import styles from './Calendar.module.css';
+
+const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+export const Calendar = () => {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const startDay = new Date(year, month - 1, 1).getDay();
+
+  const movePrevMonth = () => {
+    setCurrentDate(new Date(year, month - 2, 1));
+  };
+  const moveNextMonth = () => {
+    setCurrentDate(new Date(year, month, 1));
+  };
+
+  const dateCells = [];
+
+  const prevMonth = month === 1 ? 12 : month - 1;
+  const prevYear = month === 1 ? year - 1 : year;
+  const daysInPrevMonth = new Date(prevYear, prevMonth, 0).getDate();
+
+  for (let i = 0; i < startDay; i++) {
+    const prevDay = daysInPrevMonth - (startDay - 1 - i);
+    dateCells.push(
+      <div key={`prev-${i}`} className={styles.adjacentMonthCell}>
+        {prevDay}
+      </div>
+    );
+  }
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    dateCells.push(
+      <div key={i} className={styles.dateCell}>
+        {i}
+      </div>
+    );
+  }
+
+  const totalCells = startDay + daysInMonth;
+  const nextDays = Math.max(0, 35 - totalCells);
+
+  for (let i = 1; i <= nextDays; i++) {
+    dateCells.push(
+      <div key={`next-${i}`} className={styles.adjacentMonthCell}>
+        {i}
+      </div>
+    );
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <button onClick={movePrevMonth}>{'◀'}</button>
+        <div>
+          {year}년 {month}월
+        </div>
+        <button onClick={moveNextMonth}>{'▶'}</button>
+      </div>
+      <div className={styles.grid}>
+        {days.map(d => (
+          <div key={d} className={styles.dayLabel}>
+            {d}
+          </div>
+        ))}
+
+        {dateCells}
+      </div>
+    </div>
+  );
+};
